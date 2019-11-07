ddf29a64d5fbf54b0371b830c47f183a38b196e3
Spencer Carli <spencercarli@gmail.com>
2019-11-07 10:36:44 -0600

    Remove the basic error mention

--- App/util/api.js
+++ App/util/api.js
@@ -1,16 +1,10 @@
 const BASE_URL = "https://rns-offline-class.glitch.me";
 
 export const geoFetch = (path, options = {}) =>
-  fetch(`${BASE_URL}/api${path}`, options)
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      }
+  fetch(`${BASE_URL}/api${path}`, options).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
 
-      throw new Error("Something went wrong... please try again.");
-    })
-    .catch(error => {
-      console.warn("ERROR: ", `${BASE_URL}/api${path}`, error);
-
-      throw new Error(error);
-    });
+    throw new Error("Something went wrong... please try again.");
+  });
