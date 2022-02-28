ab09106d5414e9b55f26b4ca4618cf7869bd7423
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-02-28 16:41:02 -0600

    07 cache

--- App/util/api.js
+++ App/util/api.js
@@ -1,10 +1,32 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 const BASE_URL = "https://rns-offline-class.glitch.me";
 
-export const geoFetch = (path, options = {}) =>
-  fetch(`${BASE_URL}/api${path}`, options).then(res => {
-    if (res.ok) {
-      return res.json();
+export const geoFetch = async (path, options = {}) => {
+  const url = `${BASE_URL}/api${path}`;
+  const cacheKey = `CACHED_DATA::${url}`;
+
+  try {
+    const res = await fetch(url, options);
+
+    if (!res.ok) {
+      throw new Error("Something went wrong... please try again.");
     }
 
-    throw new Error("Something went wrong... please try again.");
-  });
+    const data = await res.json();
+
+    await AsyncStorage.setItem(
+      cacheKey,
+      JSON.stringify({
+        data,
+      })
+    );
+    const keys = await AsyncStorage.getAllKeys();
+    console.log("keys", keys);
+
+    return data;
+  } catch (error) {
+    console.log("geoFetch error", error);
+    return Promise.reject(error);
+  }
+};
